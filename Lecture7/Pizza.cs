78c0e944b5aebe6273d85f8cee3f317359149d3a
Antonio <antonioareas@me.com>
2020-10-25 12:37:12 -0700

    Add project files.

---
+++ Lecture7/Pizza.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture7 {
+    class Pizza {
+
+        //instance variables
+        private string size;
+        private int cheese;
+        private int pepperoni;
+        private int ham;
+
+        //constructor
+        public Pizza() { }
+        public Pizza (string size, int cheese, int pepperoni, int ham) {
+            this.size = size;
+            this.cheese = cheese;
+            this.pepperoni = pepperoni;
+            this.ham = ham;
+        }
+
+        //set and get size
+        public void SetSize(string size) {
+            this.size = size;
+        }
+        public string GetSize() {
+            return size;
+        }
+
+        //set and get cheese
+        public void SetCheese(int cheese) {
+            this.cheese = cheese;
+        }
+        public int GetCheese() {
+            return cheese;
+        }
+
+        //set and get pepperoni
+        public void SetPepperoni(int pepperoni) {
+            this.pepperoni = pepperoni;
+        }
+        public int GetPepperoni() {
+            return pepperoni;
+        }
+
+        //set and get ham
+        public void SetHam(int ham) {
+            this.ham = ham;
+        }
+        public int GetHam() {
+            return ham;
+        }
+
+        /// <summary>
+        /// Calculate cost of pizza
+        /// </summary>
+        /// <returns>cost</returns>
+        public double CalculateCost() {
+
+            //check pizza size
+            switch (size.ToLower()) {
+                case "small":
+                    return 10 + (2 * cheese) + (2 * pepperoni) + (2 * ham);
+                case "medium":
+                    return 12 + (2 * cheese) + (2 * pepperoni) + (2 * ham);
+                case "large":
+                    return 14 + (2 * cheese) + (2 * pepperoni) + (2 * ham);
+                default:
+                    Console.WriteLine("Unknown size");
+                    break;
+            }
+            return 0;
+
+        }
+
+        /// <summary>
+        /// Convert to a string
+        /// </summary>
+        /// <returns>object as string</returns>
+        public override string ToString() {
+            return "Size: " + size + "\nCheese: " + cheese + "\nPepperoni: " + pepperoni + "\nHam: " + ham + "\nCost: " + CalculateCost();
+        }
+
+    }
+}
---
+++ Lecture7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lecture7 {
+    class Program {
+        static void Main(string[] args) {
+
+            //test constructors, get and set
+            Pizza pizza1 = new Pizza("small", 2, 2, 3);
+            Console.WriteLine("" + pizza1.GetSize() + pizza1.GetCheese() + pizza1.GetPepperoni() + pizza1.GetHam());
+            Pizza pizza2 = new Pizza();
+            pizza2.SetSize("medium");
+            pizza2.SetCheese(4);
+            pizza2.SetPepperoni(4);
+            pizza2.SetHam(5);
+
+            //test calculate cost
+            Console.WriteLine(pizza1.CalculateCost());
+            Console.WriteLine(pizza2.CalculateCost());
+
+            //test tostring
+            Console.WriteLine(pizza1);
+            Console.WriteLine(pizza2.ToString());
+
+        }
+    }
+}
